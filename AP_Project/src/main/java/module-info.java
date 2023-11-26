0abce47273bade440cfd0612d796aad610b4d2a4
Piyush Narula <113984681+thatspiyush08@users.noreply.github.com>
2023-11-27 04:21:29 +0530

    Delete AP_Project directory

--- AP_Project/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.ap_project {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.ap_project to javafx.fxml;
-    exports com.example.ap_project;
-}
