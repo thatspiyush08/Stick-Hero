b28a61894096a0c8b0cd1873c123f2803c246989
trespasser21 <119107832+trespasser21@users.noreply.github.com>
2023-11-27 15:11:12 +0530

    Update HelloApplication.java

--- AP_Project/src/main/java/com/example/ap_project/HelloApplication.java
+++ AP_Project/src/main/java/com/example/ap_project/HelloApplication.java
@@ -4,18 +4,33 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.io.File;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private int screenCounter = 0;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/ap_project/HomeScreen.fxml"));
 
+        String path = "/C://Users//mohmm//Downloads//music//George's Lament - Go By Ocean _ Ryan McCaffrey.mp3/";
+        Media media = new Media(new File(path).toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setAutoPlay(true);
         Scene scene = new Scene(fxmlLoader.load(), 1100, 630);
+        stage.setTitle("Playing Audio");
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
 
 
     }
